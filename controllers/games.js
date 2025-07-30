e4b57987dc0f194040af4addf37ebe69c9b13da8
Dae Dowdell <dow_dell2010@yahoo.com>
2025-07-29 20:00:49 -0400

    add data functionality complete

--- controllers/games.js
+++ controllers/games.js
@@ -3,13 +3,32 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // routes==================================
-router.get('/', (req, res) => {
-  res.render('games/index.ejs');
+router.get('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    res.render('games/index.ejs', {
+      games: currentUser.games,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
-
 
 router.get('/new', async (req, res) => {
   res.render('games/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.games.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/games`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 
